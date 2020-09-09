f0669996a1ffe814c9a7472b0f6358778a0c6b06
Jakom <jakom2020@protonmail.com>
2020-09-09 08:49:17 +0100

    adding functions.py importing..

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-import json
 import discord
-
+from functions import *
 
 
 # code....
+
+
+# getToken() function callback (to store the token as variable).
+TOKEN = getToken()
+
+print(TOKEN)
