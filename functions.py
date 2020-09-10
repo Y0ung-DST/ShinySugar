0db2d323bb675f0f88ca3ba09358a725a8463238
Jakom <jakom2020@protonmail.com>
2020-09-10 09:40:48 +0100

    working on splitRoles() function

--- functions.py
+++ functions.py
@@ -1,7 +1,26 @@
 import json
 
-# ..........
-def getToken():
+
+def openRoles():
+    with open("./roles.json","r") as rr:
+        rr = json.loads(rr.read())
+    return rr
+
+def openJson():
     with open("./config.json","r") as config:
         config = json.loads(config.read())
-    return config["token"]
+    return config
+
+# ..........
+def getToken():
+    return openJson()["token"]
+
+#............
+def getPrefix():
+    return openJson()["prefix"]
+
+
+#.............
+def splitRoles(roles):
+    l = openRoles()
+    return roles
