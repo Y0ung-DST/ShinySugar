a8298ff09e39ce7425b9958c09a7420c54b29b28
Jakom <jakom2020@protonmail.com>
2020-09-10 09:41:17 +0100

    discord client connect added

--- app.py
+++ app.py
@@ -8,5 +8,16 @@ from functions import *
 
 # getToken() function callback (to store the token as variable).
 TOKEN = getToken()
+PREFIX = getPrefix()
 
 # print(TOKEN)
+class MatchingBot(discord.Client):
+    async def on_ready(self):
+        print(f"Bot is started as {self.user}!")
+    async def on_message(self, message):
+        if message.content == PREFIX + "profile":
+            await message.channel.send(splitRoles([str(i.id) for i in message.author.roles]))
+
+client = MatchingBot()
+
+client.run(TOKEN)
